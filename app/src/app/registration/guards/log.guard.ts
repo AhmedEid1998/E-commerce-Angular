3b832042f8b1a2b1b786532b6c59ed57a3ce0600
Ahmed Eid <83458048+AhmedEid1998@users.noreply.github.com>
2023-06-25 12:51:47 +0200

    update

---
+++ app/src/app/registration/guards/log.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../service/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LogGuard implements CanActivate {
+  constructor(private _AuthService:AuthService, private _Router:Router){}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      if(this._AuthService.userData.getValue() != null){
+        this._Router.navigate(['/products'])
+        return false;
+      }else{
+        return true;
+      }
+  }
+  
+}
